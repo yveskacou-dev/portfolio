7485c669a626c74132cb711cbe48e65d855ac573
Yves Arnaud KAcou <kacouyvesarnaud1@gmail.com>
2026-08-24 19:01:18 +0000

    Amelioration des competences

--- script.js
+++ script.js
@@ -58,3 +58,26 @@ function afficherTexte() {
 }
 
 afficherTexte();
+
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+elements.forEach((element) => {
+    observer.observe(element);
+});
